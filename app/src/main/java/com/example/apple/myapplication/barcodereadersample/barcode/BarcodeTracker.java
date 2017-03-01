11c99974a2b661ca25a6344300fdaf6fa30a3718
maheshostwal <mahesh_ostwal@yahoo.in>
2017-03-01 12:14:33 -0500

    MyApplication

---
+++ app/src/main/java/com/example/apple/myapplication/barcodereadersample/barcode/BarcodeTracker.java
@@ -0,0 +1,31 @@
+package com.example.apple.myapplication.barcodereadersample.barcode;
+
+/**
+ * Created by apple on 2/24/17.
+ */
+
+//package com.varvet.barcodereadersample.barcode;
+
+        import android.content.Context;
+
+        import com.google.android.gms.vision.Tracker;
+        import com.google.android.gms.vision.barcode.Barcode;
+
+class BarcodeTracker extends Tracker<Barcode> {
+    private BarcodeGraphicTrackerCallback mListener;
+
+    public interface BarcodeGraphicTrackerCallback {
+        void onDetectedQrCode(Barcode barcode);
+    }
+
+    BarcodeTracker(Context listener) {
+        mListener = (BarcodeGraphicTrackerCallback) listener;
+    }
+
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        if (item.displayValue != null) {
+            mListener.onDetectedQrCode(item);
+        }
+    }
+}
